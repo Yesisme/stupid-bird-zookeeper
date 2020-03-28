a597603b65b870f1b7947063a893d95c5f05bf86
Leyiming <1415083410@qq.com>
2020-03-28 23:57:27 +0800

    business方法提交

--- src/main/java/com/lym/zk/controller/BusinessController.java
+++ src/main/java/com/lym/zk/controller/BusinessController.java
@@ -1,5 +1,11 @@
 package com.lym.zk.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class BusinessController {
 
+    public void doBusiness(){
+        System.out.println("do business");
+    }
 }
