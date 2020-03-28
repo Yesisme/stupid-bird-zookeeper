b356fde384e68857f811b7659a0a1b2ef4601ac2
Leyiming <1415083410@qq.com>
2020-03-28 21:48:53 +0800

    first commit

---
+++ src/main/java/com/lym/zk/StupidbirdZookeeperApplication.java
@@ -0,0 +1,13 @@
+package com.lym.zk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StupidbirdZookeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StupidbirdZookeeperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lym/zk/StupidbirdZookeeperApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lym.zk;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StupidbirdZookeeperApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
